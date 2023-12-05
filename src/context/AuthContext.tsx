9892ba1d65b0d7b396ad74e1af53cfc54274dfb5
Ritik Ranjan <75849689+ritikranjan12@users.noreply.github.com>
2023-12-06 01:15:27 +0530

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -64,7 +64,7 @@ export function AuthProvider ({children} : {children : React.ReactNode}){
     }
 
     useEffect(() => {
-        const cookieFallback = localStorage.getItem("cookieFallback");
+        const cookieFallback = localStorage.getItem("cookiefallback");
         if (
           cookieFallback === "[]" ||
           cookieFallback === null ||
