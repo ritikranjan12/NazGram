894f2c4df48b862efcd13caf724896635c407335
Ritik Ranjan <75849689+ritikranjan12@users.noreply.github.com>
2023-12-06 17:23:35 +0530

    Update AuthLayout.tsx

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -3,13 +3,13 @@ import { Outlet, Navigate } from "react-router-dom";
 import { useUserContext } from "@/context/AuthContext";
 
 export default function AuthLayout() {
-  const { isAuthenticated } = useUserContext();
+  // const { isAuthenticated } = useUserContext();
 
   return (
-    <>
-      {isAuthenticated ? (
-        <Navigate to="/" />
-      ) : (
+    // <>
+    //   {isAuthenticated ? (
+    //     <Navigate to="/" />
+    //   ) : (
         <>
           <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
@@ -20,8 +20,8 @@ export default function AuthLayout() {
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
-        </>
-      )}
+{/*         </> */}
+{/*       )} */}
     </>
   );
 }
